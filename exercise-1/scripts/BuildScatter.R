c9f9f69808d9193dc4f634122c98b06781c4a482
Michael Freeman <michael.k.freeman@gmail.com>
2017-05-10 16:51:51 -0700

    Updated exercise 1

---
+++ exercise-1/scripts/BuildScatter.R
@@ -0,0 +1,10 @@
+# Function to build a scatterplot
+
+# Write your function to parameterize the following variables:
+#   - Data to use
+#   - Variable for the x axis
+#   - Variable for the y axis
+#   - Variable for the color
+#   - Title of the plot (set a default of "Title")
+#   - Label for the x axis (set a default of "X Title")
+#   - Label for the y axis (set a default of "Y Title")
